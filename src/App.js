f237a2ef8be2f69cd707694e8fb589c347a7c411
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-08-03 10:11:34 +0530

    Header Ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/UI/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header>
+
+      </Header>
     </div>
   );
 }
