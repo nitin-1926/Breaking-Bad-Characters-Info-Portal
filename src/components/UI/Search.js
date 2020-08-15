dd40eb19d8e2dcb0b9ab42e52c9d53feab14afbb
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-08-15 09:31:31 +0530

    Minor Code Change

--- src/components/UI/Search.js
+++ src/components/UI/Search.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Search = ({ getQuery }) => {
     const [text, setText] = useState('');
-    const onChangeText = (value) => {
+
+    const onChangeText = value => {
         setText(value);
         getQuery(value);
     }
+
     return (
         <section className='search'>
             <form>
@@ -19,7 +21,7 @@ const Search = ({ getQuery }) => {
                 />
             </form>
         </section>
-    )
+    );
 }
 
-export default Search
+export default Search;
