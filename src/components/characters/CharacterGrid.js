17994253777f1fdd06b90917e7323580fac53820
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-08-15 09:33:44 +0530

    Minor Code Change

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
 import CharacterItem from './CharacterItem';
 import Spinner from '../UI/Spinner';
 
 const CharacterGrid = ({ items, isLoading }) => {
     return isLoading ? <Spinner /> : <section className='cards'>
-        {items.map( item => (
-            <CharacterItem key={item.char_id} item={item}/>
-        ))}
+        {
+            items.map( item => (
+                <CharacterItem key={item.char_id} item={item} />
+            ))
+        }
     </section>
 }
 
-export default CharacterGrid
+export default CharacterGrid;
