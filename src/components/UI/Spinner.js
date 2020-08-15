0b606f69755fe6b4b14c1e4e76855a1d484a7974
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-08-15 09:28:12 +0530

    Spinner Added on Loading Code Fixed

--- src/components/UI/Spinner.js
+++ src/components/UI/Spinner.js
@@ -3,7 +3,11 @@ import spinner from '../../img/spinner.gif';
 
 const Spinner = () => {
     return (
-        <img src={spinner} style={{width: '200px', margin: 'auto', display: 'block'}} alt='Loading...' />
+        <img 
+            src={spinner} 
+            style={{width: '200px', margin: 'auto', display: 'block'}} 
+            alt='Loading...'     
+        />
     )
 }
 
