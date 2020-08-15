1f023ba79a848afc04084c749a8d7cc2d7e29b7e
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-08-15 09:05:47 +0530

    Individual Cards Updated with more values

--- src/components/characters/CharacterItem.js
+++ src/components/characters/CharacterItem.js
@@ -11,16 +11,22 @@ const CharacterItem = ({ item }) => {
           <h1>{item.name}</h1>
           <ul>
             <li>
-              <strong>Actor Name:</strong> {item.portrayed}
+              <strong>Actor Name: </strong> {item.portrayed}
             </li>
             <li>
-              <strong>Nickname:</strong> {item.nickname}
+              <strong>Nickname: </strong> {item.nickname}
             </li>
             <li>
-              <strong>Birthday:</strong> {item.birthday}
+              <strong>Birthday: </strong> {item.birthday}
             </li>
             <li>
-              <strong>Status:</strong> {item.status}
+                <strong>Occupation: </strong><br></br> {item.occupation.map( occupation => (<label>{occupation} </label>) )}
+            </li>
+            <li>
+              <strong>Status: </strong> {item.status}
+            </li>
+            <li>
+                <strong>Appearances: </strong><br></br> Season {item.appearance.map( season => (<label>{season} </label>) )}
             </li>
           </ul>
         </div>
