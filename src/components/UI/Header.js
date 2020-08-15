77e94ad12fb42a39c7a9bde4a48b74328f16f212
nitin-1926 <nitin171538.cse@chitkara.edu.in>
2020-08-15 09:32:26 +0530

    Minor Code Change

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <header className='center'>
             <img src={logo} alt='Breaking Bad' />
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
